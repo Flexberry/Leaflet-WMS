a8b1d074ef38baf379d9777d930dab8fea79fe7e
Sokolov Andrey <s.andrey-2011@yandex.ru>
2016-12-12 14:30:53 +0500

    add test exception GetFeatureInfo

--- spec/L.TileLayer.WMS.GetFeatureInfo.js
+++ spec/L.TileLayer.WMS.GetFeatureInfo.js
@@ -46,13 +46,47 @@ describe('L.TileLayer.WMS.GetFeatureInfo', function () {
 
       expect(failSpy.called).to.be.deep.equal(true);
       var str = failSpy.firstCall.args[0].toString();
-      expect(str.indexOf("Error: None of available formats for")).to.be.deep.equal(0);
+      expect(str.indexOf("Error: None of available formats for 'http://test.ru' 'GetFeatureInfo' requests are not implemented in 'L.TileLayer.WMS'. 'GetFeatureInfo' request can't be performed.")).to.be.deep.equal(0);
 
       L.TileLayer.WMS.Util.AJAX = originaAJAX;
     });
 
-    it('no intersection', function () {
+    it('no intersection infoFormat', function () {
+      var originaAJAX = L.TileLayer.WMS.Util.AJAX;
 
+      L.TileLayer.WMS.Util.AJAX = function(options) {
+        var responseText = '' +
+        '<?xml version="1.0" encoding="UTF-8"?>' +
+        '<WMS_Capabilities>' +
+        '  <Capability>' +
+        '    <Request>' +
+        '      <GetCapabilities>' +
+        '        <Format>text/xml</Format>' +
+        '      </GetCapabilities>' +
+        '      <GetFeatureInfo>' +
+        '        <Format>application/vnd.ogc.gml</Format>' +
+        '        <Format>application/vnd.ogc.gml/3.1.1</Format>' +
+        '        <Format>text/html</Format>' +
+        '        <Format>application/json</Format>' +
+        '      </GetFeatureInfo>' +
+        '    </Request>' +
+        '  </Capability>' +
+        '</WMS_Capabilities>';
+        return options.done(responseText);
+      };
+
+      var layer = new L.TileLayer.WMS('http://test.ru', {
+        version: '1.3.0',
+      });
+
+      var failSpy = sinon.spy();
+      var result = layer.getFeatureInfo({ infoFormat: 'test', fail: failSpy});
+
+      expect(failSpy.called).to.be.deep.equal(true);
+      var str = failSpy.firstCall.args[0].toString();
+      expect(str.indexOf("Error: Format 'test' ' is not implemented in 'L.TileLayer.WMS'. 'GetFeatureInfo' request can't be performed.")).to.be.deep.equal(0);
+
+      L.TileLayer.WMS.Util.AJAX = originaAJAX;
     });
 
     it('if xml then uses to GeoJSON', function () {
