82234726cb68c5d002b38ecf6665e77cc44bd831
almostcake <konstantin.mizev@gmail.com>
2017-07-31 19:45:02 +0500

    getBoundingBox for parent layer

--- examples/lib/Leaflet-WMS.js
+++ examples/lib/Leaflet-WMS.js
@@ -1,4 +1,4 @@
-/*! Leaflet-WMS 1.0.0 2017-07-28 */
+/*! Leaflet-WMS 1.0.0 2017-07-31 */
 ;(function(window, document, undefined) {
 "use strict";
 if (!String.prototype.trim) {
@@ -128,39 +128,7 @@ L.TileLayer.WMS.Util.XML = {
     return element.innerText || element.textContent || element.text;
   },
 
-  getChildElementsByTagName: function(xml, tagName) {
-    var childNodes = xml.childNodes;
-    var result = [];
-
-    for (var i = 0; i < childNodes.length; i++) {
-      if (childNodes[i].nodeName === tagName) {
-        result.push(childNodes[i]);
-      }
-    }
-
-    return result;
-  },
-
-  getElementsByPath: function(xml, path) {
-    var pathArray = path.split('/');
-    var result = L.TileLayer.WMS.Util.XML.getChildElementsByTagName(xml, pathArray[0]);
-
-    for (var i = 1; i < pathArray.length; i++) {
-      var newResult = [];
-
-      for(var j = 0; j < result.length; j++) {
-        var newElements = L.TileLayer.WMS.Util.XML.getChildElementsByTagName(result[j], pathArray[i]);
-
-        newResult = newResult.concat(newElements);
-      }
-
-      result = newResult;
-    }
-
-    return result;
-  },
-
-  normalizeElement: function(element) {
+  normalizeElement: function (element) {
     // Remove empty <text></text> elements.
     if (element.nodeType === 3 && L.TileLayer.WMS.Util.XML.getElementText(element).trim() === '' && element.parentNode) {
       element.parentNode.removeChild(element);
@@ -1122,32 +1090,19 @@ L.TileLayer.WMS.Format['text/html'] = {
 };
 
 L.TileLayer.WMS.include({
-  _boundingBoxes: null,
+  _boundingBox: null,
 
-  getBoundingBoxes: function(options) {
-    var optionsCopy = Object.assign({}, options);
-    var _this = this;
-
-    optionsCopy.done = function(capabilities, xhr) {
-      var boundingBoxes = L.TileLayer.WMS.Util.XML.getElementsByPath(capabilities, 'Capability/Layer/BoundingBox');
-      var crs = _this.options.crs || _this._map.options.crs;
-      var result;
-      
-      for (var i = 0; i < boundingBoxes.length; i++) {
-        var bbox = boundingBoxes[i];
-
-        if (bbox.getAttribute('CRS') === crs.code || bbox.getAttribute('SRS') === crs.code) {
-          var corner1 = L.latLng(bbox.getAttribute('minx'), bbox.getAttribute('miny')),
-            corner2 = L.latLng(bbox.getAttribute('maxx'), bbox.getAttribute('maxy'));
-
-          result = L.latLngBounds(corner1, corner2);
-
-          break;
-        }
+  getBoundingBox: function (options) {
+    options = L.Util.extend({
+      fail: function (errorThrown) {
+        throw errorThrown;
       }
+    }, options || {});
 
+    var _this = this;
+    var done = function (boundingBox, xhr) {
       if (typeof options.done === 'function') {
-        options.done.call(_this, result, xhr);
+        options.done.call(_this, boundingBox, xhr);
       }
 
       if (typeof options.always === 'function') {
@@ -1155,7 +1110,7 @@ L.TileLayer.WMS.include({
       }
     };
 
-    optionsCopy.fail = function(errorThrown, xhr) {
+    var fail = function (errorThrown, xhr) {
       if (typeof options.fail === 'function') {
         options.fail.call(_this, errorThrown, xhr);
       }
@@ -1165,19 +1120,71 @@ L.TileLayer.WMS.include({
       }
     };
 
-    // Check if bounding boxes was already received & cached.
-    var boundingBoxes = _this._boundingBoxes;
-    if (boundingBoxes) {
-      options.done.call(_this, boundingBoxes);
+    // Check if info format was already received & cached.
+    var boundingBox = _this._boundingBox;
+    if (boundingBox) {
+      done(boundingBox);
 
       return;
     }
 
-    // Try to send 'GetCapabilities' request & cache results for extracting bounding boxes.
-    _this.getCapabilities(optionsCopy);
+    // Try to send 'GetCapabilities' request & retrieve bounding box from capabilities.
+    _this.getCapabilities({
+      done: function (capabilities, xhr) {
+        try {
+          var leafletMap = _this._map || options.map;
+          var crs = _this.options.crs || leafletMap.options.crs;
+
+          var capabilityElement = capabilities.getElementsByTagName('Capability')[0];
+          var layerElement = capabilityElement.getElementsByTagName('Layer')[0];
+          var boundingBoxes = layerElement.getElementsByTagName('BoundingBox');
+
+          var west;
+          var east;
+          var south;
+          var north;
+          var ne;
+          var sw;
+
+          if (_this.wmsParams.version >= 1.3) {
+            // Every named Layer shall have exactly one <EX_GeographicBoundingBox> element... OGC® 06-042.
+            var geographicBoundingBox = layerElement.getElementsByTagName('EX_GeographicBoundingBox')[0];
+
+            // Extract latlongs.
+            west = L.TileLayer.WMS.Util.XML.getElementText(geographicBoundingBox.getElementsByTagName()[0], 'westBoundLongitude');
+            east = L.TileLayer.WMS.Util.XML.getElementText(geographicBoundingBox.getElementsByTagName()[0], 'eastBoundLongitude');
+            south = L.TileLayer.WMS.Util.XML.getElementText(geographicBoundingBox.getElementsByTagName()[0], 'southBoundLatitude');
+            north = L.TileLayer.WMS.Util.XML.getElementText(geographicBoundingBox.getElementsByTagName()[0], 'northBoundLatitude');
+          } else {
+            // Every Layer shall have exactly one <LatLonBoundingBox> element... OGC 01-068r3.
+            var latLonBoundingBox = layerElement.getElementsByTagName('LatLonBoundingBox')[0];
+
+            // Extract latlongs.
+            west = latLonBoundingBox.getAttribute('minx');
+            east = latLonBoundingBox.getAttribute('maxx');
+            south = latLonBoundingBox.getAttribute('miny');
+            north = latLonBoundingBox.getAttribute('maxy');
+          }
+
+          // Wrap it into latlng.
+          ne = L.latLng(north, east);
+          sw = L.latLng(south, west);
+
+
+          var boundingBox = L.latLngBounds(ne, sw);
+
+          // Cache retrieved format.
+          _this._boundingBox = boundingBox;
+
+          done(boundingBox, xhr);
+        } catch (e) {
+          fail(e, xhr);
+        }
+      },
+      fail: fail
+    });
   }
 });
-
 L.TileLayer.WMS.include({
   _capabilities: null,
 
